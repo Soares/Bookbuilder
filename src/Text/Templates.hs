21afac29e5fe8a847fe6d8beaa31ace6628920c1
Nate Soares <nate@natesoares.com>
2011-12-15 17:16:23 -0800

    broken, but pseudo-package now

--- src/Text/Templates.hs
+++ src/Text/Templates.hs
@@ -10,6 +10,7 @@ module Text.Bookbuilder.Templates
 
 import Control.Monad ( liftM, liftM2, foldM )
 import Text.ParserCombinators.Parsec
+import Data.String.Utils ( join ) -- TODO: Debug only
 
 -- TODO: handle 'any' template
 -- TODO: imports
@@ -51,13 +52,24 @@ fromBound d Unbounded = d
 fromBound d (Bounded x) = x
 -- End bound
 
+data Group = Group
+	{ groupOrder  :: Bound Integer
+	, groupMember :: Integer -> Bool
+	}
+
+-- For testing purposes
+instance Show Group where
+	show (Group b fn) = bound ++ results where
+		bound = case b of
+			Unbounded -> "@"
+			Bounded n -> show n
+		results = join "" $ map (\x -> if fn x then "!" else ".") [0..10]
+
 data Template = Template
 	{ tmplName    :: String
 	, tmplSource  :: String
 	, tmplGroups  :: Bound [Group]
 	}
-
-type Group = (Bound Integer, Integer -> Bool)
 
 instance Eq Template where
 	t1 == t2 = tmplName t1 == tmplName t2
@@ -66,20 +78,22 @@ instance Ord Template where
 	t1 <= t2 = orderings t1 <= orderings t2
 
 orderings :: Template -> Bound [Bound Integer]
-orderings = fmap (map fst) . tmplGroups
+orderings = fmap (map groupOrder) . tmplGroups
 
 tmplMatches :: Template -> [Integer] -> Bool
 tmplMatches = groupsMatch . tmplGroups
 
 groupsMatch :: Bound [Group] -> [Integer] -> Bool
 groupsMatch Unbounded _ = True
-groupsMatch (Bounded (g:gs)) (x:xs) = snd g x && groupsMatch (Bounded gs) xs
+groupsMatch (Bounded (g:gs)) (x:xs) = groupMember g x
+	&& groupsMatch (Bounded gs) xs
 
 combine :: Group -> Group -> Group
 combine = both (liftM2 (+)) (liftM2 (||)) where
-	both aaa bbb (a1, b1) (a2, b2) = (aaa a1 a2, bbb b1 b2)
+	both aaa bbb (Group a1 b1) (Group a2 b2) = Group (aaa a1 a2) (bbb b1 b2)
 
-emptyGroup = (Bounded 0, const False)
+emptyGroup = Group (Bounded 0) (const False)
+fullGroup = Group Unbounded (const True)
 
 {-
 readNameFor :: String -> [String] -> String -> Template
@@ -102,36 +116,36 @@ fst >>> snd = fst >>= (snd >>) . return
 (+>>) :: Monad m => m a -> m b -> m (a, b)
 fst +>> snd = fst >>= (\x -> snd >>= (\y -> return (x, y)))
 
-constraints = try (groups >>= return . Bounded) <|> fb
-fb = anystr >> hyphen >> return Unbounded
+constraints, fallback :: GenParser Char st (Bound [Group])
+constraints = try fallback <|> (fmap Bounded groups)
+fallback = fbmarker >> return Unbounded
 
--- TODO: should be sepBy instead of endBy?
 groups :: GenParser Char st [Group]
-groups = endBy group hyphen
+groups = endBy group underscore
 
-group, set, nums, rng, lo, hi, single :: GenParser Char st Group
-group = try uni <|> set
-set = fmap (foldr combine emptyGroup) (sepBy nums quote)
+group, nums, rng, lo, hi, single :: GenParser Char st Group
+group = do
+	chunks <- sepBy nums quote
+	return $ if null chunks then fullGroup else foldr combine emptyGroup chunks
 nums = try rng <|> try lo <|> try hi <|> single
-lo = num >>> tilda >>= \x -> return (Unbounded, (>= x))
-hi = tilda >> num >>= \y -> return (Bounded $ 1 + y, (<= y))
+lo = num >>> hyphen >>= \x -> return $ Group Unbounded (>= x)
+hi = hyphen >> num >>= \y -> return $ Group (Bounded $ 1 + y) (<= y)
 rng = do
-	(x', y') <- num >>> tilda +>> num
+	(x', y') <- num >>> hyphen +>> num
 	let (x, y) = (min x' y', max x' y')
-	return (Bounded $ 1 + y - x, (`elem` [x..y]))
-single = num >>= \n -> return (Bounded 1, (== n))
-uni = many1 underscore >> return (Unbounded, const True)
+	return $ Group (Bounded $ 1 + y - x) (`elem` [x..y])
+single = num >>= \n -> return $ Group (Bounded 1) (== n)
 
 -- | Simple data
 num :: GenParser Char st Integer
 num = fmap read $ many1 numchar
 
 -- | Simple tokens
-hyphen, tilda, quote, dot, numchar :: GenParser Char st Char
+hyphen, quote, dot, numchar :: GenParser Char st Char
 hyphen = char '-'
-tilda = char '~'
 quote = oneOf "'"
 dot = char '.'
 underscore = char '_'
 numchar = oneOf ['0'..'9']
-anystr = return "any"
+fbmarker :: GenParser Char st String
+fbmarker = mapM char "any"
